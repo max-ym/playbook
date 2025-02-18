0582a72ac1880802e3729a5282ae9769fb8402f7
Max Naumchyk <max.naumch@gmail.com>
2025-02-18 14:39:35 +0200

    Add more JS nodes

--- wasm-bind/src/project.rs
+++ wasm-bind/src/project.rs
@@ -559,14 +559,23 @@ pub struct JsRegexNodeStub {
 impl JsRegexNodeStub {
     #[wasm_bindgen(constructor)]
     pub fn from_regex(js_regex: RegExp) -> JsRegexNodeStub {
-        use lazy_regex::regex::Regex;
-        let s = String::from(js_regex.source());
-        let regex = Regex::new(&s).expect("JS thinks it is valid...");
-
+        let regex = js_regex.into_rust();
         JsRegexNodeStub {
             regex: js_regex,
             stub: canvas::NodeStub::Regex(regex).into(),
         }
+    }
+}
+
+trait IntoRegex {
+    fn into_rust(&self) -> lazy_regex::regex::Regex;
+}
+
+impl IntoRegex for RegExp {
+    fn into_rust(&self) -> lazy_regex::regex::Regex {
+        use lazy_regex::regex::Regex;
+        let s = String::from(self.source());
+        Regex::new(&s).expect("JS thinks it is valid...")
     }
 }
 
@@ -674,6 +683,483 @@ impl JsMapNodeStub {
     }
 }
 
+#[derive(Debug)]
+#[wasm_bindgen(js_name = ListNodeStub)]
+pub struct JsListNodeStub {
+    #[wasm_bindgen(readonly, getter_with_clone)]
+    pub stub: JsNodeStub,
+}
+
+#[wasm_bindgen]
+impl JsListNodeStub {
+    /// Add element to the list node stub.
+    /// This will check if the element is of the same type as the first element in the list.
+    /// If the list is empty, the element will be added.
+    ///
+    /// # Errors
+    /// If the element is not of the same type as the first element, this will throw an error.
+    #[wasm_bindgen(js_name = addElement)]
+    pub fn add_element(&mut self, element: JsDataInstance) -> Result<(), JsInvalidTypeError> {
+        if let canvas::NodeStub::List { values } = &mut self.stub.stub {
+            if let Some(first) = values.first() {
+                if !element.value.is_same_type(first) {
+                    return Err(JsInvalidTypeError {
+                        got: element.value.type_of().into(),
+                        expected: first.type_of().into(),
+                    });
+                }
+            }
+
+            values.push(element.value);
+            Ok(())
+        } else {
+            unreachable!("stub is not List, but object is ListNodeStub");
+        }
+    }
+
+    /// Get the elements in the list node stub.
+    #[wasm_bindgen(getter, js_name = elements)]
+    pub fn elements(&self) -> Vec<JsDataInstance> {
+        if let canvas::NodeStub::List { values } = &self.stub.stub {
+            values
+                .iter()
+                .map(|v| JsDataInstance { value: v.clone() })
+                .collect()
+        } else {
+            unreachable!("stub is not List, but object is ListNodeStub");
+        }
+    }
+
+    /// Remove element at the given index. If there is no element at the given index,
+    /// this will do nothing.
+    #[wasm_bindgen(js_name = removeElementAt)]
+    pub fn remove_element_at(&mut self, idx: usize) {
+        if let canvas::NodeStub::List { values } = &mut self.stub.stub {
+            if idx < values.len() {
+                let _ = values.remove(idx);
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+#[wasm_bindgen(js_name = InvalidTypeError)]
+pub struct JsInvalidTypeError {
+    #[wasm_bindgen(readonly, getter_with_clone)]
+    pub got: JsDataType,
+
+    #[wasm_bindgen(readonly, getter_with_clone)]
+    pub expected: JsDataType,
+}
+
+#[derive(Debug)]
+#[wasm_bindgen(js_name = IfElseNodeStub)]
+pub struct JsIfElseNodeStub {
+    #[wasm_bindgen(readonly, getter_with_clone)]
+    pub stub: JsNodeStub,
+}
+
+#[wasm_bindgen(js_class = IfElseNodeStub)]
+impl JsIfElseNodeStub {
+    /// Create a new if-else node stub with given amount of data inputs. Note one more first
+    /// input pin that is added on top to accept boolean value for the evaluated condition.
+    #[wasm_bindgen(constructor)]
+    pub fn new(inputs: usize) -> JsIfElseNodeStub {
+        JsIfElseNodeStub {
+            stub: canvas::NodeStub::IfElse {
+                inputs: inputs as canvas::PinOrder,
+            }
+            .into(),
+        }
+    }
+}
+
+#[derive(Debug)]
+#[wasm_bindgen(js_name = OkOrErrNodeStub)]
+pub struct JsOkOrErrNodeStub {
+    #[wasm_bindgen(readonly, getter_with_clone)]
+    pub stub: JsNodeStub,
+}
+
+#[wasm_bindgen(js_class = OkOrErrNodeStub)]
+impl JsOkOrErrNodeStub {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> JsOkOrErrNodeStub {
+        JsOkOrErrNodeStub {
+            stub: canvas::NodeStub::OkOrErr.into(),
+        }
+    }
+}
+
+#[derive(Debug)]
+#[wasm_bindgen(js_name = ValidateNodeStub)]
+pub struct JsValidateNodeStub {
+    #[wasm_bindgen(readonly, getter_with_clone)]
+    pub stub: JsNodeStub,
+}
+
+#[wasm_bindgen(js_class = ValidateNodeStub)]
+impl JsValidateNodeStub {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> JsValidateNodeStub {
+        todo!("ValidateNodeStub")
+    }
+}
+
+#[derive(Debug)]
+#[wasm_bindgen(js_name = SelectFirstNodeStub)]
+pub struct JsSelectFirstNodeStub {
+    #[wasm_bindgen(readonly, getter_with_clone)]
+    pub stub: JsNodeStub,
+}
+
+#[wasm_bindgen(js_class = SelectFirstNodeStub)]
+impl JsSelectFirstNodeStub {
+    #[wasm_bindgen(constructor)]
+    pub fn new(inputs: usize) -> JsSelectFirstNodeStub {
+        JsSelectFirstNodeStub {
+            stub: canvas::NodeStub::SelectFirst {
+                inputs: inputs as canvas::PinOrder,
+            }
+            .into(),
+        }
+    }
+}
+
+#[derive(Debug)]
+#[wasm_bindgen(js_name = ExpectOneNodeStub)]
+pub struct JsExpectOneNodeStub {
+    #[wasm_bindgen(readonly, getter_with_clone)]
+    pub stub: JsNodeStub,
+}
+
+#[wasm_bindgen(js_class = ExpectOne)]
+impl JsExpectOneNodeStub {
+    #[wasm_bindgen(constructor)]
+    pub fn new(inputs: usize) -> JsExpectOneNodeStub {
+        JsExpectOneNodeStub {
+            stub: canvas::NodeStub::ExpectOne {
+                inputs: inputs as canvas::PinOrder,
+            }
+            .into(),
+        }
+    }
+}
+
+#[derive(Debug)]
+#[wasm_bindgen(js_name = ExpectSomeNodeStub)]
+pub struct JsExpectSomeNodeStub {
+    #[wasm_bindgen(readonly, getter_with_clone)]
+    pub stub: JsNodeStub,
+}
+
+#[wasm_bindgen(js_class = ExpectSomeNodeStub)]
+impl JsExpectSomeNodeStub {
+    #[wasm_bindgen(constructor)]
+    pub fn new(msg: String) -> JsExpectSomeNodeStub {
+        JsExpectSomeNodeStub {
+            stub: canvas::NodeStub::ExpectSome { msg: msg.into() }.into(),
+        }
+    }
+}
+
+#[derive(Debug)]
+#[wasm_bindgen(js_name = MatchNodeStub)]
+pub struct JsMatchNodeStub {
+    #[wasm_bindgen(readonly, getter_with_clone)]
+    pub stub: JsNodeStub,
+}
+
+#[wasm_bindgen(js_class = MatchNode)]
+impl JsMatchNodeStub {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> JsMatchNodeStub {
+        todo!("Similar as Map, DRY code?")
+    }
+}
+
+#[derive(Debug)]
+#[wasm_bindgen(js_name = FuncNodeStub)]
+pub struct JsFuncNodeStub {
+    #[wasm_bindgen(readonly, getter_with_clone)]
+    pub stub: JsNodeStub,
+}
+
+#[wasm_bindgen(js_class = FuncNodeStub)]
+impl JsFuncNodeStub {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> JsFuncNodeStub {
+        todo!("predicates and functions?")
+    }
+}
+
+#[derive(Debug)]
+#[wasm_bindgen(js_name = ParseDateTimeNodeStub)]
+pub struct JsParseDateTimeNodeStub {
+    #[wasm_bindgen(readonly, getter_with_clone)]
+    pub format: JsString,
+
+    #[wasm_bindgen(readonly)]
+    pub date: bool,
+
+    #[wasm_bindgen(readonly)]
+    pub time: bool,
+
+    #[wasm_bindgen(readonly, getter_with_clone)]
+    pub stub: JsNodeStub,
+}
+
+#[wasm_bindgen(js_class = ParseDateTimeNodeStub)]
+impl JsParseDateTimeNodeStub {
+    fn new(format: JsString, date: bool, time: bool) -> Self {
+        let rust_format = String::from(&format).into();
+        JsParseDateTimeNodeStub {
+            format,
+            date,
+            time,
+            stub: canvas::NodeStub::ParseDateTime {
+                format: rust_format,
+                date,
+                time,
+            }
+            .into(),
+        }
+    }
+
+    #[wasm_bindgen(js_name = date)]
+    pub fn date(format: JsString) -> JsParseDateTimeNodeStub {
+        Self::new(format, true, false)
+    }
+
+    #[wasm_bindgen(js_name = time)]
+    pub fn time(format: JsString) -> JsParseDateTimeNodeStub {
+        Self::new(format, false, true)
+    }
+
+    #[wasm_bindgen(js_name = dateTime)]
+    pub fn date_time(format: JsString) -> JsParseDateTimeNodeStub {
+        Self::new(format, true, true)
+    }
+}
+
+#[derive(Debug)]
+#[wasm_bindgen(js_name = ParseMonetaryNodeStub)]
+pub struct JsParseMonetaryNodeStub {
+    #[wasm_bindgen(readonly, getter_with_clone)]
+    pub stub: JsNodeStub,
+
+    #[wasm_bindgen(readonly, getter_with_clone)]
+    pub regex: RegExp,
+}
+
+#[wasm_bindgen(js_class = ParseMonetaryNodeStub)]
+impl JsParseMonetaryNodeStub {
+    #[wasm_bindgen(constructor)]
+    pub fn new(js_regex: RegExp) -> JsParseMonetaryNodeStub {
+        let regex = js_regex.into_rust();
+        JsParseMonetaryNodeStub {
+            regex: js_regex,
+            stub: canvas::NodeStub::ParseMonetary { regex }.into(),
+        }
+    }
+}
+
+#[derive(Debug)]
+#[wasm_bindgen(js_name = ParseIntNodeStub)]
+pub struct JsParseIntNodeStub {
+    #[wasm_bindgen(readonly, getter_with_clone)]
+    pub stub: JsNodeStub,
+
+    #[wasm_bindgen(readonly, js_name = isSigned)]
+    pub is_signed: bool,
+}
+
+#[wasm_bindgen(js_class = ParseIntNodeStub)]
+impl JsParseIntNodeStub {
+    #[wasm_bindgen(constructor)]
+    pub fn new(is_signed: bool) -> JsParseIntNodeStub {
+        JsParseIntNodeStub {
+            is_signed,
+            stub: canvas::NodeStub::ParseInt { signed: is_signed }.into(),
+        }
+    }
+}
+
+#[derive(Debug)]
+#[wasm_bindgen(js_name = ConstantNodeStub)]
+pub struct JsConstantNodeStub {
+    #[wasm_bindgen(readonly, getter_with_clone)]
+    pub value: JsDataInstance,
+
+    #[wasm_bindgen(readonly, getter_with_clone)]
+    pub stub: JsNodeStub,
+}
+
+#[wasm_bindgen(js_class = ConstantNodeStub)]
+impl JsConstantNodeStub {
+    #[wasm_bindgen(constructor)]
+    pub fn new(value: JsDataInstance) -> JsConstantNodeStub {
+        let cloned = value.value.clone();
+        JsConstantNodeStub {
+            value,
+            stub: canvas::NodeStub::Constant(cloned).into(),
+        }
+    }
+}
+
+#[derive(Debug)]
+#[wasm_bindgen(js_name = OkOrCrashNodeStub)]
+pub struct JsOkOrCrashNodeStub {
+    #[wasm_bindgen(readonly, getter_with_clone)]
+    pub stub: JsNodeStub,
+
+    #[wasm_bindgen(readonly, getter_with_clone)]
+    pub message: JsString,
+}
+
+#[wasm_bindgen(js_class = OkOrCrash)]
+impl JsOkOrCrashNodeStub {
+    #[wasm_bindgen(constructor)]
+    pub fn new(msg: JsString) -> JsOkOrCrashNodeStub {
+        let rust_msg = String::from(&msg).into();
+        JsOkOrCrashNodeStub {
+            message: msg,
+            stub: canvas::NodeStub::OkOrCrash { msg: rust_msg }.into(),
+        }
+    }
+}
+
+#[derive(Debug)]
+#[wasm_bindgen(js_name = CrashNodeStub)]
+pub struct JsCrashNodeStub {
+    #[wasm_bindgen(readonly, getter_with_clone)]
+    pub message: JsString,
+
+    #[wasm_bindgen(readonly, getter_with_clone)]
+    pub stub: JsNodeStub,
+}
+
+#[wasm_bindgen(js_class = CrashNode)]
+impl JsCrashNodeStub {
+    #[wasm_bindgen(constructor)]
+    pub fn new(msg: JsString) -> JsCrashNodeStub {
+        let rust_msg = String::from(&msg).into();
+        JsCrashNodeStub {
+            message: msg,
+            stub: canvas::NodeStub::Crash { msg: rust_msg }.into(),
+        }
+    }
+}
+
+#[derive(Debug)]
+#[wasm_bindgen(js_name = UnreachableNodeStub)]
+pub struct JsUnreachableNodeStub {
+    #[wasm_bindgen(readonly, getter_with_clone)]
+    pub message: JsString,
+
+    #[wasm_bindgen(readonly, getter_with_clone)]
+    pub stub: JsNodeStub,
+}
+
+#[wasm_bindgen(js_class = UnreachableNodeStub)]
+impl JsUnreachableNodeStub {
+    #[wasm_bindgen(constructor)]
+    pub fn new(msg: JsString) -> JsUnreachableNodeStub {
+        let rust_msg = String::from(&msg).into();
+        JsUnreachableNodeStub {
+            message: msg,
+            stub: canvas::NodeStub::Unreachable { msg: rust_msg }.into(),
+        }
+    }
+}
+
+#[derive(Debug)]
+#[wasm_bindgen(js_name = TodoNodeStub)]
+pub struct JsTodoNodeStub {
+    #[wasm_bindgen(readonly, getter_with_clone)]
+    pub message: JsString,
+
+    #[wasm_bindgen(readonly, getter_with_clone)]
+    pub stub: JsNodeStub,
+}
+
+#[wasm_bindgen(js_class = TodoNodeStub)]
+impl JsTodoNodeStub {
+    #[wasm_bindgen(constructor)]
+    pub fn new(msg: JsString, inputs: usize) -> JsTodoNodeStub {
+        let rust_msg = String::from(&msg).into();
+        JsTodoNodeStub {
+            message: msg,
+            stub: canvas::NodeStub::Todo {
+                msg: rust_msg,
+                inputs: inputs as canvas::PinOrder,
+            }
+            .into(),
+        }
+    }
+}
+
+#[derive(Debug)]
+#[wasm_bindgen(js_name = CommentNodeStub)]
+pub struct JsCommentNodeStub {
+    #[wasm_bindgen(readonly, getter_with_clone)]
+    pub message: JsString,
+
+    #[wasm_bindgen(readonly, getter_with_clone)]
+    pub stub: JsNodeStub,
+}
+
+#[wasm_bindgen(js_class = CommentNodeStub)]
+impl JsCommentNodeStub {
+    #[wasm_bindgen(constructor)]
+    pub fn new(msg: JsString, inputs: usize) -> JsCommentNodeStub {
+        let rust_msg = String::from(&msg).into();
+        JsCommentNodeStub {
+            message: msg,
+            stub: canvas::NodeStub::Comment {
+                msg: rust_msg,
+                inputs: inputs as canvas::PinOrder,
+            }
+            .into(),
+        }
+    }
+}
+
+#[derive(Debug)]
+#[wasm_bindgen(js_name = NodeStubKind)]
+pub enum JsNodeStubKind {
+    File,
+    SplitBy,
+    Input,
+    Drop,
+    Output,
+    Lowercase,
+    Uppercase,
+    Strip,
+    Compare,
+    Ordering,
+    Regex,
+    Map,
+    List,
+    IfElse,
+    OkOrErr,
+    Validate,
+    SelectFirst,
+    ExpectOne,
+    ExpectSome,
+    Match,
+    Func,
+    ParseDateTime,
+    ParseMonetary,
+    ParseInt,
+    Constant,
+    OkOrCrash,
+    Crash,
+    Unreachable,
+    Todo,
+    Comment,
+}
+
 /// Node in the canvas. This is actual instance of the placed node in a project.
 #[derive(Debug)]
 #[wasm_bindgen(js_name = Node)]
@@ -747,6 +1233,8 @@ impl JsNode {
     }
 
     /// Drop the node from the canvas. You should not use the node handle after this.
+    /// All edges connected to this node will be removed, and stored metadata
+    /// removed.
     ///
     /// This also returns the stub of the node, so you can recreate it later.
     pub fn drop(self) -> Result<JsNodeStub, JsError> {
